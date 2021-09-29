1733b998ee113ce6b6f71ce9b4da41f778d60bb3
Gabriel Venancio <63819323+Gabriel-Venancio@users.noreply.github.com>
2021-09-29 15:08:54 -0300

    Scripts

---
+++ Transacional/carro.sql
@@ -0,0 +1,15 @@
+create table carro (
+	cod_carro INT PRIMARY KEY,
+	modelo VARCHAR(11),
+	preco decimal(10,2)
+);
+insert into carro (cod_carro, modelo, preco) values (1, 'Onix', 150);
+insert into carro (cod_carro, modelo, preco) values (2, 'Argo', 160);
+insert into carro (cod_carro, modelo, preco) values (3, 'HB20', 160);
+insert into carro (cod_carro, modelo, preco) values (4, 'Gol', 130);
+insert into carro (cod_carro, modelo, preco) values (5, 'Ford Ka', 150);
+insert into carro (cod_carro, modelo, preco) values (6, 'Polo', 180);
+insert into carro (cod_carro, modelo, preco) values (7, 'Sandero', 130);
+insert into carro (cod_carro, modelo, preco) values (8, 'Peugeot 208', 160);
+insert into carro (cod_carro, modelo, preco) values (9, 'Fox', 150);
+insert into carro (cod_carro, modelo, preco) values (10, 'Kwid', 160);
---
+++ Transacional/funcionario.sql
@@ -0,0 +1,25 @@
+create table funcionario (
+	cod_funcionario INT PRIMARY KEY,
+	nome VARCHAR(50),
+	sexo VARCHAR(50)
+);
+insert into funcionario (cod_funcionario, nome, sexo) values (1, 'Dianemarie', 'F');
+insert into funcionario (cod_funcionario, nome, sexo) values (2, 'Calley', 'F');
+insert into funcionario (cod_funcionario, nome, sexo) values (3, 'Elbertina', 'F');
+insert into funcionario (cod_funcionario, nome, sexo) values (4, 'Ciro', 'M');
+insert into funcionario (cod_funcionario, nome, sexo) values (5, 'Caren', 'F');
+insert into funcionario (cod_funcionario, nome, sexo) values (6, 'Venita', 'F');
+insert into funcionario (cod_funcionario, nome, sexo) values (7, 'Bryana', 'F');
+insert into funcionario (cod_funcionario, nome, sexo) values (8, 'Stanly', 'M');
+insert into funcionario (cod_funcionario, nome, sexo) values (9, 'Otha', 'F');
+insert into funcionario (cod_funcionario, nome, sexo) values (10, 'Everard', 'M');
+insert into funcionario (cod_funcionario, nome, sexo) values (11, 'Julius', 'M');
+insert into funcionario (cod_funcionario, nome, sexo) values (12, 'Kelley', 'M');
+insert into funcionario (cod_funcionario, nome, sexo) values (13, 'Constantine', 'F');
+insert into funcionario (cod_funcionario, nome, sexo) values (14, 'Delores', 'F');
+insert into funcionario (cod_funcionario, nome, sexo) values (15, 'Jelene', 'F');
+insert into funcionario (cod_funcionario, nome, sexo) values (16, 'Shelba', 'F');
+insert into funcionario (cod_funcionario, nome, sexo) values (17, 'Jedediah', 'M');
+insert into funcionario (cod_funcionario, nome, sexo) values (18, 'Colas', 'M');
+insert into funcionario (cod_funcionario, nome, sexo) values (19, 'Timoteo', 'M');
+insert into funcionario (cod_funcionario, nome, sexo) values (20, 'Valentin', 'M');
